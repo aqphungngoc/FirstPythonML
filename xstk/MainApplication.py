e8a6a6a4ef3b0b1e027ab3a1afcc0a11f746f5bc
aqphungngoc <aqphungngoc@gmail.com>
2019-06-15 18:15:04 +0700

    156

--- xstk/MainApplication.py
+++ xstk/MainApplication.py
@@ -14,3 +14,20 @@ data = pd.read_csv('./example/trip.csv')
 
 print(len(data))
 data.head()
+
+groupby_user = data.groupby('usertype').size()
+groupby_user.plot.bar(title = 'Distribution of user types')
+
+groupby_gender = data.groupby('gender').size()
+groupby_gender.plot.bar(title = 'Distribution of genders')
+
+data = data.sort_values(by='birthyear')
+groupby_birthyear = data.groupby('birthyear').size()
+groupby_birthyear.plot.bar(title = 'Distribution of birth years',
+figsize = (15,4))
+
+data_mil = data[(data['birthyear'] >= 1977) & (data['birthyear']<=1994)]
+groupby_mil = data_mil.groupby('usertype').size()
+groupby_mil.plot.bar(title = 'Distribution of user types')
+
+print(data_mil)
